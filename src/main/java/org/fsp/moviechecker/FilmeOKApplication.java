fedd02717911868421b5951e4e03e3cd70a6bcd8
François SP <francois-sp@gmx.fr>
2024-03-02 10:56:50 +0100

    Création de la vue principal et de la class de lancement

---
+++ src/main/java/org/fsp/moviechecker/FilmeOKApplication.java
@@ -0,0 +1,32 @@
+/*
+ * FilmeOKApplication.java                                  02 mars 2024
+ * IUT de Rodez, pas de droit d'auteur
+ */
+
+package org.fsp.moviechecker;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Charge e lance l'application.
+ * @author François de Saint Palais
+ */
+public class FilmeOKApplication extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(VuePrincipalControleur.class.getResource("main-view.fxml"));
+
+        Scene scene = new Scene(loader.load());
+        stage.setScene(scene);
+        stage.setTitle("FilmOK");
+
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
