b5dde064be2b6e3b878c6ac68b89fb4b2a3d7569
François SP <francois-sp@gmx.fr>
2024-03-09 00:23:10 +0100

    Gestion des erreurs pour la saisie du chemin du classeur

--- src/main/java/org/fsp/filmok/VuePrincipalControleur.java
+++ src/main/java/org/fsp/filmok/VuePrincipalControleur.java
@@ -6,6 +6,7 @@
 package org.fsp.filmok;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.FileChooser;
@@ -16,6 +17,13 @@ import java.io.File;
  * @author François de Saint Palais
  */
 public class VuePrincipalControleur {
+    private static final Alert ALERT_CHEMIN_CLASSEUR_VIDE
+            = new Alert(Alert.AlertType.ERROR, "Chemin de classeur vide");
+    private static final Alert ALERT_CLASSEUR_EXTENSION_INVALIDE
+            = new Alert(Alert.AlertType.ERROR, "Le fichier doit être un classeur Excel ou LibreOffice");
+    private static final Alert ALERT_CLASSEUR_INEXISTANT
+            = new Alert(Alert.AlertType.ERROR, "Classeur inexistant. Vérifiez le chemin");
+
     @FXML
     private TextField inputCheminFichierExcel;
 
@@ -30,7 +38,37 @@ public class VuePrincipalControleur {
     public void handleValider() {
         // TODO
         System.out.println("Valider");
-        System.out.println(inputCheminFichierExcel.getText());
+        String cheminClasseurSaisie = inputCheminFichierExcel.getText();
+        System.out.println(cheminClasseurSaisie);
+        if (estClasseurValide(cheminClasseurSaisie)) {
+            chargerClasseur(cheminClasseurSaisie);
+        }
+    }
+
+    private void chargerClasseur(String cheminClasseurSaisie) {
+        // TODO
+    }
+
+    /**
+     * Vérifie si le chemin du classeur est valide
+     * Affiche une boite de dialogue avec un message d'erreur s'il n'est pas valide
+     * @param cheminClasseur
+     * @return
+     */
+    private boolean estClasseurValide(String cheminClasseur) {
+        boolean estValide = true;
+        if (cheminClasseur == null || cheminClasseur.isEmpty() || cheminClasseur.isBlank()) {
+            estValide = false;
+            ALERT_CHEMIN_CLASSEUR_VIDE.showAndWait();
+        } else if (   ! cheminClasseur.endsWith(".xlsx")
+                   && ! cheminClasseur.endsWith(".ods")){
+            estValide = false;
+            ALERT_CLASSEUR_EXTENSION_INVALIDE.showAndWait();
+        } else if (! new File(cheminClasseur).exists()) {
+            estValide = false;
+            ALERT_CLASSEUR_INEXISTANT.showAndWait();
+        }
+        return estValide; // TODO STUB
     }
 
     @FXML
