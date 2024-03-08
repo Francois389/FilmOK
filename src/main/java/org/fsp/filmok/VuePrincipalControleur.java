72f132ec914a5777dec6bcca65153b77d6646fde
François SP <francois-sp@gmx.fr>
2024-03-08 13:43:20 +0100

    Ajout de la selection du classeur via l'explorateur de fichier

--- src/main/java/org/fsp/filmok/VuePrincipalControleur.java
+++ src/main/java/org/fsp/filmok/VuePrincipalControleur.java
@@ -8,13 +8,16 @@ package org.fsp.filmok;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 /**
  * @author François de Saint Palais
  */
 public class VuePrincipalControleur {
     @FXML
-    private TextField inputCHeminFichierExcel;
+    private TextField inputCheminFichierExcel;
 
     @FXML
     private TableView<Film> tableFilms;
@@ -27,6 +30,21 @@ public class VuePrincipalControleur {
     public void handleValider() {
         // TODO
         System.out.println("Valider");
+        System.out.println(inputCheminFichierExcel.getText());
+    }
+
+    @FXML
+    public void handleParcourir() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choisir un fichier Excel");
+        fileChooser.getExtensionFilters().addAll(
+        new FileChooser.ExtensionFilter("Classeur Excel", "*.xlsx"),
+        new FileChooser.ExtensionFilter("LibreOffice Classeur", "*.ods"),
+        new FileChooser.ExtensionFilter("All Files", "*.*"));
+        File file = fileChooser.showOpenDialog(null);
+        if (file != null) {
+            inputCheminFichierExcel.setText(file.getAbsolutePath());
+        }
     }
 
     @FXML
