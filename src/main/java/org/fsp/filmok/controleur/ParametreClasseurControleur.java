fe9128db90a7924fefaef58e382d3383731655d1
François SP <francois-sp@gmx.fr>
2024-03-09 21:57:08 +0100

    Vue paramètre classeur

---
+++ src/main/java/org/fsp/filmok/controleur/ParametreClasseurControleur.java
@@ -0,0 +1,26 @@
+/*
+ * ParametreClasseurControleur.java                                  09 mars 2024
+ * IUT de Rodez, pas de droit d'auteur
+ */
+
+package org.fsp.filmok.controleur;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
+/**
+ * @author François de Saint Palais
+ */
+public class ParametreClasseurControleur {
+    @FXML
+    public ComboBox colonnesTitre;
+    @FXML
+    public ComboBox colonnesDateSorti;
+    @FXML
+    public ComboBox colonnesRealisateur;
+    @FXML
+    public ComboBox colonnesDuree;
+    @FXML
+    public ComboBox colonnesResume;
+
+}
